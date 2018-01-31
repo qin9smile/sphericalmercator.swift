f94d1b6bf81f4f7540ae8fa23a5d9ef36a018bf1
charlotte <charlotte@gagogroup.com>
2018-01-31 11:11:50 +0800

    add pixel and coordinate convertion

--- sphericalmercator.swift
+++ sphericalmercator.swift
@@ -35,7 +35,34 @@ class SphericalMercator {
     }
   }
 
-  func px(coordinate: CLLocationCoordinate2D, zoom: Double) -> CLLocationCoordinate2D {
-    return CLLocationCoordinate2D()
+  /// Convert lon lat to screen pixel value
+  func px(coordinate: CLLocationCoordinate2D, zoom: Int) -> CGPoint? {
+    guard let cacheSize = cache[size] else {
+      return nil
+    }
+
+    let d = cacheSize.zc[zoom]
+    let f = min(max(sin(D2R * coordinate.latitude), -0.9999), 0.9999)
+    var x = round(d + coordinate.longitude * cacheSize.Bc[zoom])
+    var y = round(d + 0.5 * log((1 + f) / (1 - f)) * (-cacheSize.Cc[zoom]))
+    if x > cacheSize.Ac[zoom] {
+      x = cacheSize.Ac[zoom]
+    }
+
+    if y > cacheSize.Ac[zoom] {
+      y = cacheSize.Ac[zoom]
+    }
+    return CGPoint(x: x, y: y)
+  }
+
+  /// Convert screen pixel value to Coordinate
+  func ll(px: CGPoint, zoom: Int) -> CLLocationCoordinate2D? {
+    guard let cacheSize = cache[size] else {
+      return nil
+    }
+    let g = (Double(px.y) - cacheSize.zc[zoom]) / (-cacheSize.Cc[zoom])
+    let longitude = (Double(px.x) - cacheSize.zc[zoom]) / cacheSize.Bc[zoom]
+    let latitude = R2D * (2 * atan(exp(g)) - 0.5 * Double.pi)
+    return CLLocationCoordinate2D.init(latitude: latitude, longitude: longitude)
   }
 }
